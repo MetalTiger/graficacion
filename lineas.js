741bd575753f74f7851ae1e929ae6676f1d81f7a
Kevin <kevinkamacho47@gmail.com>
2022-02-12 13:24:29 -0700

    Terminar prueba de lineas

--- lineas.js
+++ lineas.js
@@ -21,9 +21,9 @@ function main()  // Función principal
         dragging = false;
 
         if (document.getElementById('Basica').checked) {
-                
-            if (pStart.x > pEnd.x || pStart.y > pEnd.y) {
             
+            if (pStart.x > pEnd.x || pStart.y > pEnd.y) {
+                
                 drawBasicLine(context, pEnd.x, pEnd.y, pStart.x, pStart.y, "#197BBD"); // Se dibuja la linea
                 
             }else{
@@ -41,7 +41,7 @@ function main()  // Función principal
             drawBresenhamLine(context, pStart.x, pStart.y, pEnd.x, pEnd.y, "#FF0000");
 
         }
-      
+    
 
     });
 
@@ -53,6 +53,89 @@ function main()  // Función principal
             pEnd = windowToCanvasCoord(canvas, e.clientX, e.clientY);
 
         }
+    });
+
+    var botonProbar = document.getElementById("Prueba");
+    botonProbar.addEventListener("click", function(e){
+
+        var espacio = 10;
+
+        if (document.getElementById('Basica').checked) {
+         
+
+            console.time("Basico");
+
+            for (let i = 0; i < 1000; i++) {
+                
+                if((i%espacio) === 0){
+
+                    drawBasicLine(context, 0, i, 1000, i, "#197BBD"); // Horizontal
+                    drawBasicLine(context, i, 0, i, 1000, "#197BBD"); // Vertical                    
+                    drawBasicLine(context, i, 0, 0, i, "#197BBD"); // Esquina superior izquierda
+                    drawBasicLine(context, i, 1000, 1000, i, "#197BBD"); // Esquina inferior derecha
+                    drawBasicLine(context, 0, i, 1000-i, 1000, "#197BBD"); // Esquina inferior izquierda
+                    drawBasicLine(context, i, 0, 1000, 1000-i, "#197BBD"); // Esquina superior derecha
+
+                    
+                } 
+
+            }
+
+            console.timeEnd("Basico");
+            
+        } else if(document.getElementById('DDA').checked){
+
+            console.time("DDA");
+            
+            for (let i = 0; i < 1000; i++) {
+                
+                if((i%espacio) === 0){
+
+                    drawDDALine(context, 0, i, 1000, i, "#00FF00"); // Horizontal
+                    drawDDALine(context, i, 0, i, 1000, "#00FF00"); // Vertical                    
+                    drawDDALine(context, i, 0, 0, i, "#00FF00"); // Esquina superior izquierda
+                    drawDDALine(context, i, 1000, 1000, i, "#00FF00"); // Esquina inferior derecha
+                    drawDDALine(context, 0, i, 1000-i, 1000, "#00FF00"); // Esquina inferior izquierda
+                    drawDDALine(context, i, 0, 1000, 1000-i, "#00FF00"); // Esquina superior derecha
+
+                    
+                }
+
+            }
+
+            console.timeEnd("DDA");
+
+        } else {
+
+            console.time("Bresenham");
+            
+            for (let i = 0; i < 1000; i++) {
+                
+                if((i%espacio) === 0){
+
+                    drawBresenhamLine(context, 0, i, 1000, i, "#FF0000"); // Horizontal
+                    drawBresenhamLine(context, i, 0, i, 1000, "#FF0000"); // Vertical                    
+                    drawBresenhamLine(context, i, 0, 0, i, "#FF0000"); // Esquina superior izquierda
+                    drawBresenhamLine(context, i, 1000, 1000, i, "#FF0000"); // Esquina inferior derecha
+                    drawBresenhamLine(context, 0, i, 1000-i, 1000, "#FF0000"); // Esquina inferior izquierda
+                    drawBresenhamLine(context, i, 0, 1000, 1000-i, "#FF0000"); // Esquina superior derecha
+
+                    
+                }
+
+            }
+
+            console.timeEnd("Bresenham");
+
+        }
+
+    });
+
+    var botonBorrar = document.getElementById("Borrar");
+    botonBorrar.addEventListener("click", function(e){
+
+        context.clearRect(0, 0, 1000, 1000);
+
     });
 
 }
